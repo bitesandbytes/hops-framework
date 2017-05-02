3d777f10ec90ade949b03ff6193e9033f79c0bdd
JS Suhas <j.s.suhas@live.in>
2017-05-03 03:21:56 +0530

    Started work on embedding learner (using autoencoders, on tflearn; using stub code from github.com/tflearn/autoencoder.py) Will now work on writing the full actor module for Asti/Hexapod.

--- distributed_learner/parameter_client.py
+++ distributed_learner/parameter_client.py
@@ -1,10 +1,10 @@
 class ParameterClient:
-    def __init__(server_ip, server_port, param_dims):
+    def __init__(self, server_ip, server_port, param_dims):
         self.server_ip = server_ip
         self.server_port = server_port
         # Init client sockets
         # Init client Nd-arrays
 
-    def send_updates(update_deltas):
+    def send_updates(self, update_deltas):
         # send update deltas
         # if send_count == K, receive new params
--- distributed_learner/parameter_server.py
+++ distributed_learner/parameter_server.py
@@ -1,25 +1,25 @@
 class ParameterServer:
-    def __init__(bind_port, param_dims, max_update_count):
+    def __init__(self, bind_port, param_dims, max_update_count):
         # Init server
         # Init params Nd-array
         # Init update rate : alpha
         pass
 
-    def start_server():
+    def start_server(self):
         # bind port
         # start multithreaded server
         pass
 
-    def stop_server():
+    def stop_server(self):
         # return params
         # stop all active threads
 
-    def set_update_rate(new_update_rate):
+    def set_update_rate(self, new_update_rate):
         # lock update rate variable
         # set update rate
         # unlock variable
 
-    def _server_thread_function(thread_id):
+    def _server_thread_function(self, thread_id):
         # lock update rate
         # lock param array
         # lock update_count
